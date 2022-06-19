dffbad781a0927008e07942910f71bc1f682e190
sbb <84119280+helloobaby@users.noreply.github.com>
2022-06-19 21:18:39 +0800

    判断一个文件夹内的所有git仓库有没有有commit

---
+++ git-helper.ps1
@@ -0,0 +1,33 @@
+﻿#
+#Changes not staged for commit
+#
+#
+#
+#
+
+
+
+
+Start-Transcript   #写入日志文件
+$alldir = Get-ChildItem -Directory
+
+foreach ($dir in $alldir){ #对每个文件夹执行下面的命令
+    cd $dir
+    $newalldir = Get-ChildItem -Directory -s -h
+
+    foreach ($newdir in $newalldir){
+        if ($newdir.Name -eq '.git'){
+        echo "[+]Find Git repository-----------------------------"
+        echo $dir.Name
+        git status 
+        echo "[-]------------------------------------------------"
+        break
+    }
+    }
+
+    
+    cd ..
+}
+
+
+Stop-Transcript #
